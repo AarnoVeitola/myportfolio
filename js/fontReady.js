3b525db86efd4acadb46c36494e7ea72d4f2be2a
Aarno Veitola <aarnoveitola@Aarnos-Air.lan>
2025-02-23 15:28:34 +0200

    Fixed the delay with loading fonts

---
+++ js/fontReady.js
@@ -0,0 +1,10 @@
+if (document.fonts) {
+    document.fonts.ready.then(function() {
+        document.documentElement.style.visibility = 'visible';
+    });
+} else {
+    // Fallback for browsers that don't support the Font Loading API:
+    window.onload = function() {
+        document.documentElement.style.visibility = 'visible';
+    };
+}
